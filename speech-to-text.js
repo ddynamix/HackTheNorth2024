0b52e9c84cd2affa856b0f192c50994b0e9d3038
SquareLord <abhicoolsuperstar@gmail.com>
2024-09-14 10:25:42 -0400

    added text shortening for efficiency whilst translating to braille

--- speech-to-text.js
+++ speech-to-text.js
@@ -1,28 +1,47 @@
+const Groq = require("groq-sdk");
+const fs = require("fs");
 require('dotenv').config();
 
-const body = new FormData();
-body.append('file', 'https://output.lemonfox.ai/wikipedia_ai.mp3');
-body.append('language', 'english');
-body.append('response_format', 'json');
 
 let description;
-console.log(process.env.LEMONFOX_API_KEY);
+const groq_api_key = process.env.GROQ_API_KEY;
 
-fetch('https://api.lemonfox.ai/v1/audio/transcriptions', {
-  method: 'POST',
-  headers: {
-    'Authorization': 'Bearer: ' + process.env.LEMONFOX_API_KEY
-  },
-  body: body
-})
-.then(response => response.json()).then(data => {
-  description = data['text'];
-  
-})
-.catch(error => {
-  console.error('Error:', error);
-});
+const groq = new Groq({apiKey: process.env.GROQ_API_KEY});
 
-//description = long-form speech-to-text
+
+const chatCompletion = async () => {
+
+}
+
+async function main() {
+  const transcription = await groq.audio.transcriptions.create({
+    file: fs.createReadStream("wikipedia_ai.mp3"), // Required path to audio file - replace with your audio file!
+    model: "distil-whisper-large-v3-en", // Required model to use for transcription
+    language: "en", // Optional
+    temperature: 0.0, // Optional
+  });
+
+  console.log(transcription.text);
+  description = transcription.text;
+
+  const response = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "you are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: `${description} - Shorten this sentence to about 25% of its original length, while still prioritizing keeping detail.`,
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+  console.log(response.choices[0].message.content);
+}
+
+main();
+
+
 
 
