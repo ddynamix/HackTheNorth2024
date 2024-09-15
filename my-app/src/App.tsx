ff5a188ad9b425b451b0292e093aadd3724efdcf
GabeYou <85317995+GabeYou@users.noreply.github.com>
2024-09-15 07:26:25 -0400

    changed the app appearance

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,16 @@
 import './App.css';
 import SingleFileUploader from './components/fileupload';
+import LogoHTN from './assets/LogoHTN.png'; // Make sure to update the path to the actual location of your logo file
 
 function App() {
   return (
     <div className="app-container">
       <header className="app-header">
-        <h1>PERCEIV/IO</h1>
+        <img src={LogoHTN} alt="PERCEIV/IO Logo" />
       </header>
       
       <main className="app-main">
-      <h2>Upload your MP4 File</h2>
+        <h2>Upload your MP4 File</h2>
         <SingleFileUploader />
       </main>
       <footer className="app-footer">
